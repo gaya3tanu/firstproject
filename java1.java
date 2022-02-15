8f6eec5510c7aa0859c35952079cd0222d8ba828
Vinod <Vinod@LAPTOP-FHBU8CS2>
2022-02-15 13:50:45 -0500

    commit3

---
+++ java1.java
@@ -0,0 +1,14 @@
+package practice1;
+
+public class java1 {
+
+	public static void main(String[] args) {
+		  System.out.println("Hello gayathri");
+		  int x = 5;
+		    int y = 3;
+		    System.out.println(x + y);
+	  System.out.println(x - y);
+
+	}
+
+}
